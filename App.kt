b44f546818ab67f461d4f58b1195076c9ed9d284
Gourob Mazumder <gourob.mazumder@enosisbd.com>
2025-03-14 00:58:47 +0600

    make a simple hello world project and added testing

---
+++ App.kt
@@ -0,0 +1,7 @@
+fun main(){
+  println("Hello, CI/CD")
+}
+
+fun add(a: Int, b: Int): Int {
+  return a +b 
+}
---
+++ AppTest.kt
@@ -0,0 +1,9 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class AppTest {
+  @Test
+  fun testAddition() {
+    assertEquals(5, add(2,3))
+  }
+}
